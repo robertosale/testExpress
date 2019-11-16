0a7c7a8ea6af11c3a86d9dbe2228f928d1f5d866
Roberto Sale <robertito.sale@gmail.com>
2019-11-15 21:33:50 -0300

    base ajax app no styles

---
+++ index.js
@@ -0,0 +1,64 @@
+const mysql = require('mysql');
+const express = require('express');
+const bodyparser = require('body-parser');
+const url = require('url');
+
+
+let app = express();
+app.use(bodyparser.json());
+
+app.use(express.static('testAjax'));
+
+
+let mysqlConnection = mysql.createConnection({
+    host:'localhost',
+    user: 'root',
+    password: 'Lapicera123!',
+    database: 'EJ9',
+   
+
+});
+
+mysqlConnection.connect((err)=>{
+    if(!err)console.log("The connection to the DataBase was succesfull");
+    else console.log("Error in the DataBase connection: \n" + JSON.stringify(err,undefined,2));
+});
+
+app.listen(3000,()=>{
+    console.log("Express is running at port 3000");
+})
+
+
+app.get('/request',(req,res)=> {
+    var result;
+    
+    mysqlConnection.query(`SELECT nombre, anio FROM Pelicula
+                        WHERE LOWER(nombre) LIKE \'%${req.query.peli.toLowerCase()}%\'`,(err,result,fields)=>{
+        
+        res.send(result);
+        console.log(result);
+
+    });
+
+
+});
+
+
+
+
+////Esta prueba me devolvía todo el array de json de peliculas
+
+// app.get('/request',(req,res)=> {
+//     var result;
+//     mysqlConnection.query(`SELECT * FROM Pelicula `,(err,result,fields)=>{
+        
+        // Object.keys(result).forEach((key)=>{      //Esto recorre cada uno de los elementos del array result
+        //     let row = result[key];
+        //     console.log(row);
+           
+        // });
+//         res.send(result);
+
+//     });
+   
+//});
---
+++ testAjax/script.js
@@ -0,0 +1,32 @@
+function cargarPeliculas(){
+    
+    let xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = ()=>{
+        if(xhttp.readyState == 4 && xhttp.status ==200){
+            
+            let jackson = JSON.parse(xhttp.responseText);
+            Object.keys(jackson).forEach((key)=>{      //Esto recorre cada uno de los elementos del array result
+                let row = jackson[key];
+                console.log(row.nombre);
+
+                document.getElementById('resultados').innerHTML += `<h1> ${row.nombre}     ${row.anio} </h1>`;
+               
+            });
+
+
+            
+        }
+    }
+
+
+    let url = new URL('http://127.0.0.1:3000/request');  //armo la url, esta tes la base
+    url.searchParams.set('peli',document.getElementById('inputPeli').value); //sigo armando la url de busqueda
+
+    console.log(url);
+    xhttp.open("GET",url,true);
+    xhttp.withCredentials=true;
+    xhttp.send();
+
+   
+    
+}
